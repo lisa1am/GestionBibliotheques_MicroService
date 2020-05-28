70bc326d03be78f700b4b8d91a733c3c4aa5cd3c
«lisa1am» <laitmouloud@gmail.com>
2020-05-28 13:34:14 +0200

    verifiying connexion before launching

--- src/main/java/fr/dauphine/miage/Biblio/BibliothequeApplication.java
+++ src/main/java/fr/dauphine/miage/Biblio/BibliothequeApplication.java
@@ -1,6 +1,9 @@
 package fr.dauphine.miage.Biblio;
 
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,47 +13,147 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class BibliothequeApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		SpringApplication.run(BibliothequeApplication.class, args);
 
-		System.out.println("CA MARCHEEEEE !");
+		/*System.out.println("CA MARCHEEEEE !");
 
 		URL obj = null;
 		try {
-			obj = new URL("http://localhost:8000/livres/isbn/1");
-		HttpURLConnection con = null;
-		con = (HttpURLConnection) obj.openConnection();
+			obj = new URL("http://localhost:8003/livres/isbn/1");
+			HttpURLConnection con = null;
+			con = (HttpURLConnection) obj.openConnection();
 
 
-		con.setRequestMethod("GET");
-		con.setRequestProperty("User-Agent", "Mozilla/5.0");
-		int responseCode = con.getResponseCode();
-		System.out.println("GET Response Code :: " + responseCode);
-		if (responseCode == HttpURLConnection.HTTP_OK) { // success
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("User-Agent", "Mozilla/5.0");
+			int responseCode = con.getResponseCode();
+			System.out.println("GET Response Code : " + responseCode);
+			if (responseCode == HttpURLConnection.HTTP_OK) { // success
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						con.getInputStream()));
+				String inputLine;
+				StringBuffer response = new StringBuffer();
 
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
+				while ((inputLine = in.readLine()) != null) {
+					response.append(inputLine);
+				}
+				in.close();
+
+
+				// print result
+				System.out.println(response.toString());
+				JSONObject jo = new JSONObject(response.toString());
+				String titre = jo.getString("titre");
+				System.out.println(titre);
+			} else {
+				System.out.println("GET request not worked");
 			}
-			in.close();
-
-			// print result
-			System.out.println(response.toString());
-		} else {
-			System.out.println("GET request not worked");
-		}
-		} catch (IOException e) {
+		} catch (IOException | JSONException e) {
 			e.printStackTrace();
+		}*/
+
+		boolean endProgram = false;
+		boolean bonchoix = false;
+		int str = 0;
+		ArrayList<Integer> choix = new ArrayList<Integer>();
+		int nbchoix = 4;
+		for(int i = 1;i<=nbchoix;i++){
+			choix.add(i);
 		}
 
+
+
+		System.out.println("\n\n\nConnexion au service de gestion des livres ... ");
+		System.out.println("...........");
+		URL urlLivre = new URL("http://localhost:8003/h2-console");
+		HttpURLConnection conLivre = null;
+		conLivre = (HttpURLConnection) urlLivre.openConnection();
+		int responseCode = conLivre.getResponseCode();
+		if (responseCode == HttpURLConnection.HTTP_OK) {
+			System.out.println("Connexion établie !\n\n\n");
+		}else{
+			System.out.println(responseCode+" : Impossible d'atteindre le service. Veuillez vérifier qu'il est bien démarré.");
+		}
+
+		System.out.println("\n\n\nConnexion au service de gestion des lecteurs ... ");
+		System.out.println("...........");
+		URL urlLecteur = new URL("http://localhost:8002/h2-console");
+		HttpURLConnection conLecteur = null;
+		conLecteur = (HttpURLConnection) urlLecteur.openConnection();
+		responseCode = conLecteur.getResponseCode();
+		if (responseCode == HttpURLConnection.HTTP_OK) {
+			System.out.println("Connexion établie !\n\n\n");
+		}else{
+			System.out.println(responseCode+" : Impossible d'atteindre le service. Veuillez vérifier qu'il est bien démarré.");
+		}
+
+		System.out.println("\n\n\nConnexion au service de gestion des prêts ... ");
+		System.out.println("...........");
+		URL urlPret = new URL("http://localhost:8001/h2-console");
+		HttpURLConnection conPret = null;
+		conPret = (HttpURLConnection) urlPret.openConnection();
+		responseCode = conPret.getResponseCode();
+		if (responseCode == HttpURLConnection.HTTP_OK) {
+			System.out.println("Connexion établie !\n\n\n");
+		}else{
+			System.out.println(responseCode+" : Impossible d'atteindre le service. Veuillez vérifier qu'il est bien démarré.");
+		}
+
+
+
+
+
+			while(!endProgram){
+			System.out.println("1. Ajoutez un lecteur");
+			System.out.println("2. Consulter les lecteurs");
+			System.out.println("3. Consulter l'historique des prêts");
+			System.out.println("4. Exit");
+
+			while(!bonchoix){
+				Scanner sc = new Scanner(System.in);
+				System.out.println("Veuillez faire un choix :");
+				str = sc.nextInt();
+
+				if(choix.contains(str)){
+					bonchoix = true;
+				}
+				else{
+					System.out.println("ERROR Veuillez faire un choix parmis les propositions ci-dessous");
+					System.out.println("1. Ajoutez un lecteur");
+					System.out.println("2. Consulter les lecteurs");
+					System.out.println("3. Consulter l'historique des prêts");
+					System.out.println("4. Exit");
+				}
+			}
+
+			if(str == 1){
+				System.out.println("Exécuter le choix 1");
+				// Code get lecteur
+
+
+			}
+			if(str == 2){
+				System.out.println("Exécuter le choix 2");
+			}
+			if(str == 3){
+				System.out.println("Exécuter le choix 3");
+			}
+			if(str == 4){
+				endProgram = true;
+			}
+			bonchoix = false;
+		}
+
+		System.out.println("fin du scenario !!");
 
 	}
 
