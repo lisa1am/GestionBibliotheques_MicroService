c6e9f3c0b6295502d8620b5228e26f52496823db
Samuel <sam.arzur@gmail.com>
2020-05-28 12:45:19 +0200

    Scénarion interface mise en place v1

--- src/main/java/fr/dauphine/miage/Biblio/BibliothequeApplication.java
+++ src/main/java/fr/dauphine/miage/Biblio/BibliothequeApplication.java
@@ -10,6 +10,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class BibliothequeApplication {
@@ -22,7 +25,7 @@ public class BibliothequeApplication {
 
 		URL obj = null;
 		try {
-			obj = new URL("http://localhost:8000/livres/isbn/1");
+			obj = new URL("http://localhost:8003/livres/isbn/1");
 		HttpURLConnection con = null;
 		con = (HttpURLConnection) obj.openConnection();
 
@@ -30,7 +33,7 @@ public class BibliothequeApplication {
 		con.setRequestMethod("GET");
 		con.setRequestProperty("User-Agent", "Mozilla/5.0");
 		int responseCode = con.getResponseCode();
-		System.out.println("GET Response Code :: " + responseCode);
+		System.out.println("GET Response Code : " + responseCode);
 		if (responseCode == HttpURLConnection.HTTP_OK) { // success
 			BufferedReader in = new BufferedReader(new InputStreamReader(
 					con.getInputStream()));
@@ -51,6 +54,54 @@ public class BibliothequeApplication {
 			e.printStackTrace();
 		}
 
+		boolean endProgram = false;
+		boolean bonchoix = false;
+		int str = 0;
+		ArrayList<Integer> choix = new ArrayList<Integer>();
+		int nbchoix = 4;
+		for(int i = 1;i<=nbchoix;i++){
+			choix.add(i);
+		}
+
+		while(!endProgram){
+			System.out.println("1. Ajoutez un lecteur");
+			System.out.println("2. Consulter les lecteurs");
+			System.out.println("3. Consulter l'historique des prêts");
+			System.out.println("4. Exit");
+
+			while(!bonchoix){
+				Scanner sc = new Scanner(System.in);
+				System.out.println("Veuillez faire un choix :");
+				str = sc.nextInt();
+
+				if(choix.contains(str)){
+					bonchoix = true;
+				}
+				else{
+					System.out.println("ERROR Veuillez faire un choix parmis les propositions ci-dessous");
+					System.out.println("1. Ajoutez un lecteur");
+					System.out.println("2. Consulter les lecteurs");
+					System.out.println("3. Consulter l'historique des prêts");
+					System.out.println("4. Exit");
+				}
+			}
+
+			if(str == 1){
+				System.out.println("Exécuter le choix 1");
+			}
+			if(str == 2){
+				System.out.println("Exécuter le choix 2");
+			}
+			if(str == 3){
+				System.out.println("Exécuter le choix 3");
+			}
+			if(str == 4){
+				endProgram = true;
+			}
+			bonchoix = false;
+		}
+
+		System.out.println("fin du scenario !!");
 
 	}
 
